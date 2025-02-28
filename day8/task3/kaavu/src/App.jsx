cf7bafb0b8805bd020376a153e72a06b92a37a91
visalachi36 <visalachivisalini@gmail.com>
2025-02-28 16:21:09 +0530

    Add files via upload

---
+++ day8/task3/kaavu/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import StudentName from './StudentName';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Student Portal</h1>
+      <StudentName name="VISAL" />
+    </div>
+  );
+};
+
+export default App;
