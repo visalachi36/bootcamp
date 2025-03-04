1961c9f22c7743024ded7b3c4f2755476e6f00c2
visalachi36 <visalachivisalini@gmail.com>
2025-03-04 12:26:40 +0530

    Delete TASK2 directory

--- TASK2/server/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getStudents, addStudent } = require("../controllers/studentController");
-
-const router = express.Router();
-
-router.get("/", getStudents);
-router.post("/", addStudent);
-
-module.exports = router;
